f495239133f11821728186b9dd589f6210e64b5e
Krokodili <luga.geri@gmail.com>
2021-11-23 23:40:37 +0100

    Challenge 2

--- src/index.js
+++ src/index.js
@@ -1,27 +1,61 @@
-function addCard(pokemonName,imgSrc,hp,att,def,spcAtt,spcDef,speed,games){
+function addCard(pokemonName,imgSrc,hp,att,def,spcAtt,spcDef,speed,games,sprites){
 
     //Capitalize first letter of pokemon name cuz it was annoying me that it wasnt the same as your photo.
-    
+
     function capitalizeFirstLetter(string) {
         return string[0].toUpperCase() + string.slice(1);
     }
     let name = capitalizeFirstLetter(pokemonName)
 
     //Main li
-    const cardLiEl = document.createElement('li')
-    cardLiEl.setAttribute('class','card')
-    document.querySelector('ul.cards').append(cardLiEl)
+    function createMainLi(){
+        const cardLiEl = document.createElement('li')
+        cardLiEl.setAttribute('class','card')
+        document.querySelector('ul.cards').append(cardLiEl)
+        return cardLiEl
+    }
+    const cardLiEl = createMainLi()
     //H2
+    function createH2El(name){
     const h2El = document.createElement('h2')
     h2El.setAttribute('class','card--title')
     h2El.textContent= name
     cardLiEl.append(h2El)
+    }
+    createH2El(name)
     //Img
     const imgEl = document.createElement('img')
     imgEl.setAttribute('width',256)
     imgEl.setAttribute('class','card--img')
     imgEl.setAttribute('src',imgSrc)
     cardLiEl.append(imgEl)
+    let imgNr = 0
+    setInterval(() => {
+        switch (imgNr) {
+            case 0:
+                imgEl.setAttribute('src', sprites.back_default)
+                imgNr++
+                break;
+            case 1:
+                imgEl.setAttribute('src', sprites.back_shiny)
+                imgNr++
+                break;
+            case 2:
+                imgEl.setAttribute('src', sprites.front_default)
+                imgNr++
+                break;
+            case 3:
+                imgEl.setAttribute('src', sprites.front_shiny)
+                imgNr++
+                break;
+            case 4:
+                imgEl.setAttribute('src', imgSrc)
+                imgNr=0
+                break;
+            default:
+                break;
+        }
+    }, 2000);
     //ul
     const ulEl = document.createElement('ul')
     ulEl.setAttribute('class','card--text')
@@ -56,5 +90,5 @@ for(let element of data){
     for(let game of element.game_indices){
         gamesText += game.version.name + ", "
     }
-    addCard(element.name,element.sprites.other['official-artwork'].front_default,element.stats[0].base_stat,element.stats[1].base_stat,element.stats[2].base_stat,element.stats[3].base_stat,element.stats[4].base_stat,element.stats[5].base_stat,gamesText)
+    addCard(element.name,element.sprites.other['official-artwork'].front_default,element.stats[0].base_stat,element.stats[1].base_stat,element.stats[2].base_stat,element.stats[3].base_stat,element.stats[4].base_stat,element.stats[5].base_stat,gamesText,element.sprites)
 }
